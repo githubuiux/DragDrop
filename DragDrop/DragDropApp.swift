ee9badbcf6eb17a476bd86ce02b6dfff640c7ed1
githubuiux <uiuxista@gmail.com>
2023-07-26 11:09:01 +0300

    Initial Commit

---
+++ DragDrop/DragDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropApp.swift
+//  DragDrop
+//
+//  Created by Levent on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct DragDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
